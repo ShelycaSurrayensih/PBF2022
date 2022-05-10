095609064a615fe60283f63d5bce6bdf026ba90d
ShelycaSurrayensih <shelysurrayensih0501@gmail.com>
2022-05-10 19:55:18 +0700

    praktikum firebase

---
+++ Pertemuan 10/web-reactku/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyB4CXfr92NMOZnwJ80keGk9R8G73lMzEk0",
+    authDomain: "fir-shely.firebaseapp.com",
+    projectId: "fir-shely",
+    storageBucket: "fir-shely.appspot.com",
+    messagingSenderId: "980652532075",
+    appId: "1:980652532075:web:18db8145ade81c6fedac2e",
+    measurementId: "G-D1FSZWBF1C"
+  };
