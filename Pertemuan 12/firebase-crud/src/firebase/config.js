b234f5f217d1d12f71f73a17975ce4e641749b28
ShelycaSurrayensih <shelysurrayensih0501@gmail.com>
2022-06-02 14:34:17 +0700

    Modul 12 - Operasi CRUD menggunakan Firebase Realtime Database

---
+++ Pertemuan 12/firebase-crud/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    // Your config values
+    apiKey: "AIzaSyClKMSKbgCcf58jfg045tb2cv29ehl36fQ",
+    authDomain: "fir-login-shelyca.firebaseapp.com",
+    projectId: "fir-login-shelyca",
+    storageBucket: "fir-login-shelyca.appspot.com",
+    messagingSenderId: "966934968015",
+    appId: "1:966934968015:web:b7b98675ecdf6f31cba9ba",
+    measurementId: "G-HQPMKNGLMR"
+  };
+
+export default firebaseConfig;
